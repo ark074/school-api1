3f0d8ea27ff7037c83a720511f16d594cf009c77
Ranjith kumar <ranjithkumarabba@gmail.com>
2025-05-24 09:26:07 +0530

    Update app.js

--- school-api1/app.js
+++ school-api1/app.js
@@ -4,12 +4,20 @@ require('dotenv').config();
 const app = express();
 const schoolRoutes = require('./routes/schools');
 
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Debug middleware to log request bodies
+app.use((req, res, next) => {
+  console.log('Incoming request body:', req.body);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('School API is running');
 });
 
+// Use the school routes
 app.use('/api', schoolRoutes);
 
 // Error handling middleware
